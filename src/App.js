49a475ed48fc6778680ca6f51a4d884c64970ca9
Brian Smith <brian.bubbam@gmail.com>
2021-09-20 12:33:36 -0500

    updated app.js with imported components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Profile from './components/Profile'
+import Meetups from './components/Meetups'
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      
+      <Switch>
+        <Route path='/Profile' >
+          <Profile />
+        </Route>
+        <Route path='/Login' >
+          <Login />
+        </Route>
+        <Route path='/Meetups' >
+          <Meetups />
+        </Route>
+
+      </Switch>
+      <NavBar />
     </div>
   );
 }
